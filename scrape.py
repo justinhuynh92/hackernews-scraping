dfc4b2fccf899618d196f154ffc94218c8eca812
Justin Huynh <justinhuynh639@gmail.com>
2023-09-19 16:03:01 -0700

    testing selectors

--- scrape.py
+++ scrape.py
@@ -3,4 +3,7 @@ from bs4 import BeautifulSoup
 
 #get information from page
 res = requests.get('https://news.ycombinator.com/news')
-print(res.text)
+soup = BeautifulSoup(res.text, 'html.parser')
+links = soup.select('.storylink')
+votes = soup.select('.score')
+print(votes[0].get('id'))
