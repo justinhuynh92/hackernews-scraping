1da171c7a7c3c9b680956002bdaa673012e8470a
Justin Huynh <justinhuynh639@gmail.com>
2023-09-19 16:29:35 -0700

    hacker news 2

--- scrape.py
+++ scrape.py
@@ -1,23 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 #get information from page
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.titleline > a')
-votes = soup.select('.score')
+subtext = soup.select('.subtext')
 
 #create function that receives links and votes
-def create_custom_hn(links, votes):
+def create_custom_hn(links, subtext):
     hn = []
     for idx, item in enumerate(links):
         #grab title of each link
-        title = links[idx].getText()
-        href = links[idx].get('href', None)
-        #just show the points without the text after
-        points = int(votes[idx].getText().replace(' points', ''))
-        print(points)
-        hn.append({'title': title, 'link': href})
+        title = item.getText()
+        href = item.get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            #just show the points without the text after
+            points = int(vote[0].getText().replace(' points', ''))
+            #only get output with points at last 100
+            if points > 99:
+                hn.append({'title': title, 'link': href, 'votes': points})
     return hn
 
-create_custom_hn(links, votes)
+#make the output neater to read
+pprint.pprint(create_custom_hn(links, subtext))
