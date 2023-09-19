4fc732b10d408b2d0730c7df8984bf4b0c6e374e
Justin Huynh <justinhuynh639@gmail.com>
2023-09-19 16:19:50 -0700

    hacker news 1

--- scrape.py
+++ scrape.py
@@ -4,6 +4,20 @@ from bs4 import BeautifulSoup
 #get information from page
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
-links = soup.select('.storylink')
+links = soup.select('.titleline > a')
 votes = soup.select('.score')
-print(votes[0].get('id'))
+
+#create function that receives links and votes
+def create_custom_hn(links, votes):
+    hn = []
+    for idx, item in enumerate(links):
+        #grab title of each link
+        title = links[idx].getText()
+        href = links[idx].get('href', None)
+        #just show the points without the text after
+        points = int(votes[idx].getText().replace(' points', ''))
+        print(points)
+        hn.append({'title': title, 'link': href})
+    return hn
+
+create_custom_hn(links, votes)
